27e0ff3ba469c59021b29d717307bbc3b421bfbc
UnknownArt01 <sriskanadi@student.ciputra.ac.id>
2023-05-16 15:30:16 +0700

    first commit and adding login (not fix)

--- public/index.php
+++ public/index.php
@@ -1,163 +1,55 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pendaftaran Siswa Baru SMA Zion</title>
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css">
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Http\Request;
 
-    <style>
-        .container1 {
-            margin: auto;
-            width: fit-content;
-            padding-top: 10%;
-        }
+define('LARAVEL_START', microtime(true));
 
-        .container2 {
-            margin: auto;
-            width: fit-content;
-        }
+/*
+|--------------------------------------------------------------------------
+| Check If The Application Is Under Maintenance
+|--------------------------------------------------------------------------
+|
+| If the application is in maintenance / demo mode via the "down" command
+| we will load this file so that any pre-rendered content can be shown
+| instead of starting the framework, which could cause an exception.
+|
+*/
 
-        .container3 {
-            display: flex;
-            align-items: center;
-        }
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
 
-        .p1 {
-            margin-right: 10px;
-        }
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| this application. We just need to utilize it! We'll simply require it
+| into the script here so we don't need to manually load our classes.
+|
+*/
 
-        .btn {
-            display: inline-block;
-            padding: 10px 20px;
-            font-size: 16px;
-            font-weight: bold;
-            text-align: center;
-            text-decoration: none;
-            background-color: #45a049;
-            color: white;
-            border: none;
-            border-radius: 5px;
-            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
-            transition: all 0.3s ease;
-        }
+require __DIR__.'/../vendor/autoload.php';
 
-        .btn:hover {
-            background-color: #70c174;
-            box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
-            transform: scale(1.1);
-            color: white;
-        }
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can handle the incoming request using
+| the application's HTTP kernel. Then, we will send the response back
+| to this client's browser, allowing them to enjoy our application.
+|
+*/
 
-        table {
-            width: 100%;
-            border-collapse: collapse;
-        }
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
-        th,
-        td {
-            padding: 8px;
-            text-align: left;
-            border-bottom: 1px solid #ddd;
-        }
-    </style>
+$kernel = $app->make(Kernel::class);
 
-    <script>
-        function generateRandomNumber() {
-            // Generate a random 12-digit number
-            var randomNumber = Math.floor(Math.random() * 900000000000) + 100000000000;
+$response = $kernel->handle(
+    $request = Request::capture()
+)->send();
 
-            // Update the number in the HTML element
-            document.getElementById("randomNumber").textContent = randomNumber;
-        }
-
-        // Generate a random number when the page loads
-        window.addEventListener('load', function() {
-            generateRandomNumber();
-        });
-
-        function generateRandomPassword() {
-            // Define characters for password generation
-            var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+=-';
-
-            // Define password length
-            var passwordLength = 12;
-
-            // Initialize an empty password string
-            var password = '';
-
-            // Generate random characters to form the password
-            for (var i = 0; i < passwordLength; i++) {
-                var randomIndex = Math.floor(Math.random() * characters.length);
-                password += characters.charAt(randomIndex);
-            }
-
-            // Update the password in the HTML element
-            document.getElementById("randomPassword").textContent = password;
-        }
-
-        // Generate a random password when the page loads
-        window.addEventListener('load', function() {
-            generateRandomPassword();
-        });
-    </script>
-
-</head>
-
-<body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container">
-            <a class="navbar-brand" href="#">Website Pendaftaran Siswa Baru SMA Zion</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <!-- <li class="nav-item">
-                        <a class="nav-link" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">About</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Services</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Contact</a>
-                    </li> -->
-                </ul>
-            </div>
-        </div>
-    </nav>
-
-
-    <!-- Nomor Formulir -->
-    <div class="container1 mt-4">
-        <h1>Nomor Formulir Anda</h1>
-    </div>
-
-    <div class="container2 mt-4">
-        <h1 id="randomNumber"></h1>
-    </div>
-
-    <!-- Auto Password -->
-    <div class="container2 mt-4">
-        <div class="container3">
-            <p>Password Anda: </p>
-            <p> &nbsp; </p>
-            <p id="randomPassword"></p>
-        </div>
-    </div>
-
-    <!-- Button Isi Biodata -->
-    <div class="container2 mt-4">
-        <a href="http://127.0.0.1:8000/isiBiodata.php" class="btn">Isi Biodata</a>
-    </div>
-
-    <!-- Bootstrap JS -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-
-</html>
+$kernel->terminate($request, $response);
