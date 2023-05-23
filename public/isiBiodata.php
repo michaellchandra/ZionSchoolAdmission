417773918889dc87be7d66007a045b0ecf16166a
JoWi2404 <joeywiryawan2@gmail.com>
2023-05-23 13:18:35 +0700

    Update isiBiodata.php

--- public/isiBiodata.php
+++ public/isiBiodata.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bootstrap Website</title>
+    <title>Isi Biodata</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.min.css">
 </head>
 
